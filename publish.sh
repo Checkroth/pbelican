176a4d10f613af33b83a9f0893ce2c74688eafb2
Charles Heckroth <checkroth@gmail.com>
2023-05-06 10:23:42 +0900

    upgrade pelican, fix ghpages script

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-git checkout master && git branch -D gh-pages && git checkout -b gh-pages && pelican && ghp-import -n output/ && git push origin gh-pages -f && git checkout -
+git checkout main && git branch -D gh-pages && git checkout -b gh-pages && pelican && ghp-import -n output/ && git push origin gh-pages -f && git checkout -
